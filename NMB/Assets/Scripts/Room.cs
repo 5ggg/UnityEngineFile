422c611466d5dca9c28cd5ccd3984990f2207b7d
JiaLiang Song <1278968765@qq.com>
2020-08-20 01:44:57 +0800

    update generator

---
+++ NMB/Assets/Scripts/Room.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Room : MonoBehaviour
+{
+
+    public GameObject doorLeft, doorRight, doorUp, doorDown;
+
+    public bool roomLeft, roomRight, roomUp, roomDown;
+
+
+    void Start()
+    {
+        doorLeft.SetActive(roomLeft);
+        doorRight.SetActive(roomRight);
+        doorUp.SetActive(roomUp);
+        doorDown.SetActive(roomDown);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ NMB/Assets/Scripts/RoomGenerator.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RoomGenerator : MonoBehaviour
+{
+    public enum Direction { up, down, left, right };
+    public Direction direction;
+
+
+    [Header("Room_Info")]
+    public GameObject roomPrefab;
+    public int roomNumber;
+    public Color startColor, endColor;
+    private GameObject endRoom;
+
+    [Header("Position_Control")]
+    public Transform generatorPoint;
+    public float xOffset;
+    public float yOffset;
+    public LayerMask roomLayer;
+
+    public List<Room> rooms = new List<Room>();
+
+    void Start()
+    {
+        for (int i = 0; i < roomNumber; i++)
+        {
+            rooms.Add(Instantiate(roomPrefab, generatorPoint.position, Quaternion.identity).GetComponent<Room>());
+
+            //改变point位置
+            ChangePointPos();
+        }
+
+        rooms[0].GetComponent<SpriteRenderer>().color = startColor;
+
+        endRoom = rooms[0].gameObject;
+        foreach (var room in rooms)
+        {
+
+            //if (room.transform.position.sqrMagnitude > endRoom.transform.position.sqrMagnitude)
+            //{
+            //    endRoom = room.gameObject;
+            //}
+            SetupRoom(room, room.transform.position);
+        }
+        endRoom.GetComponent<SpriteRenderer>().color = endColor;
+    }
+
+    void Update()
+    {
+        if (Input.anyKeyDown)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
+
+    public void ChangePointPos()
+    {
+        do
+        {
+            direction = (Direction)Random.Range(0, 4);
+
+            switch (direction)
+            {
+                case Direction.up:
+                    generatorPoint.position += new Vector3(0, yOffset, 0);
+                    break;
+                case Direction.down:
+                    generatorPoint.position += new Vector3(0, -yOffset, 0);
+                    break;
+                case Direction.left:
+                    generatorPoint.position += new Vector3(-xOffset, 0, 0);
+                    break;
+                case Direction.right:
+                    generatorPoint.position += new Vector3(xOffset, 0, 0);
+                    break;
+            }
+        } while (Physics2D.OverlapCircle(generatorPoint.position, 0.2f, roomLayer));
+    }
+
+    public void SetupRoom(Room newRoom, Vector3 roomPosition)
+    {
+        newRoom.roomUp = Physics2D.OverlapCircle(roomPosition + new Vector3(0, yOffset, 0), 0.2f, roomLayer);
+        newRoom.roomDown = Physics2D.OverlapCircle(roomPosition + new Vector3(0, -yOffset, 0), 0.2f, roomLayer);
+        newRoom.roomLeft = Physics2D.OverlapCircle(roomPosition + new Vector3(-xOffset, 0, 0), 0.2f, roomLayer);
+        newRoom.roomRight = Physics2D.OverlapCircle(roomPosition + new Vector3(xOffset, 0, 0), 0.2f, roomLayer);
+
+    }
+}
