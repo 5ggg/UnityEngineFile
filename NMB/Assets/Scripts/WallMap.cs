f8c9d93ef34ecc1b3f17d6f8a37f002bc2b7d8df
JiaLiang Song <1278968765@qq.com>
2020-08-20 16:47:27 +0800

    17_minimap

---
+++ NMB/Assets/Scripts/WallMap.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallMap : MonoBehaviour
+{
+
+    GameObject mapSprite;
+
+    private void OnEnable()
+    {
+        mapSprite = transform.parent.GetChild(0).gameObject;
+
+        mapSprite.SetActive(false);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            mapSprite.SetActive(true);
+        }
+    }
+}
